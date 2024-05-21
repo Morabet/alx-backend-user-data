5654af23c813d691f986a88dca5c14764ddce8e7
Morabet <ismailelmorabet2@gmail.com>
2024-05-21 13:54:31 +0000

    fixing app

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -34,13 +34,13 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app_views.errorhandler(401)
+@app.errorhandler(401)
 def unauthorized_error_handler(error):
     """ Custom error handler for 401 Unauthorized """
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app_views.errorhandler(403)
+@app.errorhandler(403)
 def forbidden_error_handler(error):
     """ Custom error handler for 403 Forbidden """
     return jsonify({"error": "Forbidden"}), 403
