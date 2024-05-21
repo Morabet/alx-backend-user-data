8d9c7ef93e9b4ef6683615c7b39460819762e51a
Morabet <ismailelmorabet2@gmail.com>
2024-05-21 14:01:02 +0000

    fox app before request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -52,7 +52,7 @@ def before_request_func():
     path = request.path
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if auth is None or path not in excluded_paths:
+    if auth:
         if auth.require_auth(path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
